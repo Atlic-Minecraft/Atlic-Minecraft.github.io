2683fb7e3620c0719f5c67919a04283ad7855afb
iEnis <enis-neimi@outlook.de>
2024-12-10 03:47:57 +0100

    removed unused div's and added random comment for testing

--- footer/footer.js
+++ footer/footer.js
@@ -1,8 +1,8 @@
 const footer = document.createElement("div");
 footer.className = "footer";
 footer.innerHTML = `
-    <div><a class="outline link" href="mailto:support@atlic.net"><i class="fas fa-envelope icon"></i> Contact Us</a></div>
-    <div><a class="outline link" href="https://discord.gg/cQgjrusqD9"><i class="fab fa-discord icon"></i> Discord</a></div>
+    <a class="outline link" href="mailto:support@atlic.net"><i class="fas fa-envelope icon"></i> Contact Us</a>
+    <a class="outline link" href="https://discord.gg/cQgjrusqD9"><i class="fab fa-discord icon"></i> Discord</a>
 `;
 
 document.body.appendChild(footer);
